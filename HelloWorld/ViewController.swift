e0542868f1d94d5f4d67ea4406b0200d2c27968b
k.kuda <k_kuda@kyototool.co.jp>
2022-08-26 12:31:01 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by KTC次世代 on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
